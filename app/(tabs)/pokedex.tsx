e6f856e60ae26f8197cdf70015106eddf151fb4b
dr1602 <david.arangelg@gmail.com>
2025-06-22 21:25:22 -0600

    feat: adds initial structure to call the api with structure

--- app/(tabs)/pokedex.tsx
+++ app/(tabs)/pokedex.tsx
@@ -1,8 +1,24 @@
-import { getPokeApi } from '@/chore/api/pokeApi';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Text, View } from 'react-native';
 
+import { getPokeApi, getPokemonDetailsByUrlApi } from '@/chore/api/pokeApi';
+
+interface PokemonUrlType {
+  url: string;
+}
+
+interface PokemonData {
+  id: string;
+  name: string;
+  type: any;
+  order?: string;
+  sprites?: any;
+}
+
 export const Pokedex = () => {
+  const [pokemons, setPokemons] = useState<PokemonData[]>([]);
+
+  console.log('Pokemons --->', pokemons);
   useEffect(() => {
     (async () => {
       await loadPokemons();
@@ -12,7 +28,26 @@ export const Pokedex = () => {
   const loadPokemons = async () => {
     try {
       const response = await getPokeApi();
-      console.log(response);
+      const pokemonsArray: PokemonData[] = [];
+
+      for await (const pokemon of response.results as PokemonUrlType[]) {
+        const pokemonDetails: PokemonData = await getPokemonDetailsByUrlApi(
+          pokemon.url
+        );
+
+        console.log(pokemon.url);
+        console.log(pokemonDetails);
+
+        pokemonsArray.push({
+          id: pokemonDetails.id,
+          name: pokemonDetails.name,
+          type: pokemonDetails.type[0].type.name,
+          order: pokemonDetails.order,
+          sprites: pokemonDetails.sprites.other['home'].front_default,
+        });
+      }
+
+      setPokemons([...pokemons, ...pokemonsArray]);
     } catch (error) {
       console.error(error);
     }
--- chore/api/pokeApi.ts
+++ chore/api/pokeApi.ts
@@ -1,14 +1,24 @@
-import { API_HOST } from '@/utils/constants/constants'
+import { API_HOST } from '@/utils/constants/constants';
 
-const getPokeApi = async() => {
-    try{
-        const url =  `${API_HOST}/pokemon?limit=20&offset=0`
-        const response = await fetch(url)
-        const result = await response.json()
-        return result
-    }catch(error){
-        throw(error)
-    }
-}
+const getPokeApi = async () => {
+  try {
+    const url = `${API_HOST}/pokemon?limit=20&offset=0`;
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
 
-export {getPokeApi}
+const getPokemonDetailsByUrlApi = async (url: string) => {
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export { getPokeApi, getPokemonDetailsByUrlApi };
