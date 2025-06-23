aed4c11e28c2f969ace67fe18fe2e1b9eec191c5
dr1602 <david.arangelg@gmail.com>
2025-06-22 19:15:12 -0600

    feat: adds function to build petition to api

---
+++ chore/api/pokeApi.ts
@@ -0,0 +1,14 @@
+import { API_HOST } from '@/utils/constants/constants'
+
+const getPokeApi = async() => {
+    try{
+        const url =  `${API_HOST}/pokemon?limit=20&offset=0`
+        const response = await fetch(url)
+        const result = await response.json()
+        return result
+    }catch(error){
+        throw(error)
+    }
+}
+
+export {getPokeApi}
