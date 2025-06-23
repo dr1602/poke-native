06304c4143d3501ba4c0a609fc17e75988b5b843
dr1602 <david.arangelg@gmail.com>
2025-06-22 19:58:25 -0600

    feat: creates first succesful api call

--- app/(tabs)/pokedex.tsx
+++ app/(tabs)/pokedex.tsx
@@ -1,6 +1,23 @@
+import { getPokeApi } from '@/chore/api/pokeApi';
+import { useEffect } from 'react';
 import { Text, View } from 'react-native';
 
 export const Pokedex = () => {
+  useEffect(() => {
+    (async () => {
+      await loadPokemons();
+    })();
+  }, []);
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokeApi();
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <View>
       <Text> Pokedex</Text>
