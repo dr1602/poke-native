0c860c702250c3088ee35bc7f1b9abec0da8fc6b
dr1602 <david.arangelg@gmail.com>
2025-06-29 15:16:39 -0600

    feat: creates a typed function for dynamic colour selection` background

--- utils/getColourByPokemonType.ts
+++ utils/getColourByPokemonType.ts
@@ -1,3 +1,8 @@
-export const getColourByPokemonType = () => {
-    
-}
+import { POKEMON_TYPE_COLORS } from '@/utils/constants/colourConstants';
+
+type PokemonType = keyof typeof POKEMON_TYPE_COLORS;
+
+export const getColourByPokemonType = (types: string): string => {
+  const normalizedType = types.toLowerCase() as PokemonType;
+  return POKEMON_TYPE_COLORS[normalizedType] ?? '#000000';
+};
