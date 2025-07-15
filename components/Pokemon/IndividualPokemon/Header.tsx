8d332c90e0457dfd14ed8384c5a0e766f1fa274c
dr1602 <david.arangelg@gmail.com>
2025-07-14 18:17:15 -0600

    feat: adds pokemon image, background and name, with styles

--- components/Pokemon/IndividualPokemon/Header.tsx
+++ components/Pokemon/IndividualPokemon/Header.tsx
@@ -1,5 +1,6 @@
-import { Image, SafeAreaView, StyleSheet, View } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 
+import { capitalize } from '@/utils/capitalize';
 import { getColourByPokemonType } from '@/utils/getColourByPokemonType';
 import { PokemonFinalData } from '@/utils/types/PokeTypes';
 
@@ -8,32 +9,59 @@ interface PokemonDataProps {
 }
 
 export const Header = ({ PokemonData }: PokemonDataProps) => {
-  //   const pokemonData: PokemonFinalData | undefined = PokemonData;
-  const color = getColourByPokemonType(PokemonData.types);
+  const PokemonColour: string = getColourByPokemonType(PokemonData.types);
+  const bgStyles = { backgroundColor: PokemonColour, ...styles.Background };
 
   return (
-    <View>
-      <SafeAreaView>
+    <>
+      <View style={bgStyles} />
+      <SafeAreaView style={styles.safeArea}>
         <View style={styles.imageContainer}>
           <Image source={{ uri: PokemonData.image }} style={styles.image} />
         </View>
+        <View style={styles.header}>
+          <Text style={styles.name}> {capitalize(PokemonData.name)} </Text>
+        </View>
       </SafeAreaView>
-    </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    marginHorizontal: 20,
+    marginTop: 39,
+  },
   imageContainer: {
     flex: 1,
     justifyContent: 'center',
-    alignContent: 'center',
+    alignItems: 'center',
     top: 30,
-    paddingLeft: 69,
-    paddingRight: 69,
   },
   image: {
     width: 250,
     height: 250,
     resizeMode: 'contain',
+  },
+  header: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 51,
+  },
+  name: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 21,
+  },
+  Background: {
+    width: '100%',
+    height: 375,
+    justifyContent: 'center',
+    position: 'absolute',
+    borderBottomEndRadius: 690,
+    borderBottomLeftRadius: 300,
+    transform: [{ scaleX: 2 }],
   },
 });
