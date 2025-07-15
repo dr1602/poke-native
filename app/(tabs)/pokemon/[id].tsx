f46a01dd2c835a082dc34fd0bb660aca4a5af451
dr1602 <david.arangelg@gmail.com>
2025-07-15 11:51:28 -0600

    fix: adds error handling validation

--- app/(tabs)/pokemon/[id].tsx
+++ app/(tabs)/pokemon/[id].tsx
@@ -1,4 +1,4 @@
-import { ScrollView } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
 import { Header } from '@/components/Pokemon/IndividualPokemon/Header';
 import { usePokemonDetailStore } from '@/store/pokemonDetailStore';
@@ -6,12 +6,46 @@ import { PokemonFinalData } from '@/utils/types/PokeTypes';
 
 const Pokemon = () => {
   const { currentPokemonData } = usePokemonDetailStore();
+  let pokemonData: PokemonFinalData | undefined;
+
+  if (!!currentPokemonData) {
+    try {
+      pokemonData = currentPokemonData;
+    } catch (e) {
+      console.error('Error al parsear los detalles del Pokemon:', e);
+    }
+  }
+
+  if (!pokemonData) {
+    return (
+      <View>
+        <Text style={styles.errorText}>
+          Cargando o no se encontró la información del Pokémon.
+        </Text>
+      </View>
+    );
+  }
 
   return (
     <ScrollView>
-      <Header PokemonData={currentPokemonData as PokemonFinalData} />
+      <Header PokemonData={pokemonData} />
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  PokemonName: {
+    color: 'white',
+  },
+  PokemonId: {
+    color: 'white',
+  },
+  errorText: {
+    paddingHorizontal: 18,
+    color: 'red',
+    fontSize: 18,
+    textAlign: 'center',
+  },
+});
 
 export default Pokemon;
