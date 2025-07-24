63de701c8c58c2fd3dfee8bb2306e7fec2427350
dr1602 <david.arangelg@gmail.com>
2025-07-24 14:27:54 -0600

    refactor: modifies codes to accept multiple types

--- components/Pokemon/IndividualPokemon/Types.tsx
+++ components/Pokemon/IndividualPokemon/Types.tsx
@@ -4,16 +4,30 @@ import { usePokemonDetailStore } from '@/store/pokemonDetailStore';
 
 export const Types = () => {
   const { currentPokemonData } = usePokemonDetailStore();
+
+  console.log(currentPokemonData);
   return (
     <>
-      <View>
-        <Text style={styles.text}> {currentPokemonData?.types} </Text>
+      <View style={styles.content}>
+        <Text style={styles.text}>
+          {/* {
+          Array(currentPokemonData?.types).map((item) => {
+            <View>
+              
+            </View>
+          })
+          }  */}
+        </Text>
       </View>
     </>
   );
 };
 
 const styles = StyleSheet.create({
+  content: {
+    paddingHorizontal: 20,
+    marginTop: 21,
+  },
   text: {
     color: '#fff',
   },
