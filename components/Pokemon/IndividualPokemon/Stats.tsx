68b83dc79e7a0e6620996316d3ccd040589d189a
dr1602 <david.arangelg@gmail.com>
2025-07-25 16:38:59 -0600

    refactor: modifies the structure of the Stats for future additions

--- components/Pokemon/IndividualPokemon/Stats.tsx
+++ components/Pokemon/IndividualPokemon/Stats.tsx
@@ -1,18 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native';
 
 import { usePokemonDetailStore } from '@/store/pokemonDetailStore';
+import { capitalize } from '@/utils/capitalize';
 
 export const Stats = () => {
   const { currentPokemonData } = usePokemonDetailStore();
 
   return (
     <View style={styles.content}>
-      <Text style={styles.title}> Estadísticas Base </Text>
       {currentPokemonData?.stats?.map((item) => {
         return (
           <View key={item.stat.name} style={styles.block}>
             <View style={styles.blockTitle}>
-              <Text style={styles.text}> {item.stat.name} </Text>
+              <Text style={styles.text}> {capitalize(item.stat.name)} </Text>
             </View>
             <Text style={styles.text}>BARRA</Text>
           </View>
@@ -27,12 +27,6 @@ const styles = StyleSheet.create({
     paddingHorizontal: 18,
     marginTop: 9,
     marginBottom: 9,
-  },
-  title: {
-    color: '#fff',
-    fontWeight: 'bold',
-    fontSize: 18,
-    paddingBottom: 3,
   },
   block: {
     flexDirection: 'row',
