217fb74fbc22355c78dbda8d5222cc1293f001e1
dr1602 <david.arangelg@gmail.com>
2025-07-25 16:36:46 -0600

    feat: adds further components structure

--- components/Pokemon/IndividualPokemon/Stats.tsx
+++ components/Pokemon/IndividualPokemon/Stats.tsx
@@ -1,26 +1,47 @@
-import { usePokemonDetailStore } from '@/store/pokemonDetailStore';
 import { StyleSheet, Text, View } from 'react-native';
+
+import { usePokemonDetailStore } from '@/store/pokemonDetailStore';
 
 export const Stats = () => {
   const { currentPokemonData } = usePokemonDetailStore();
 
-  console.log(currentPokemonData);
   return (
     <View style={styles.content}>
       <Text style={styles.title}> Estadísticas Base </Text>
+      {currentPokemonData?.stats?.map((item) => {
+        return (
+          <View key={item.stat.name} style={styles.block}>
+            <View style={styles.blockTitle}>
+              <Text style={styles.text}> {item.stat.name} </Text>
+            </View>
+            <Text style={styles.text}>BARRA</Text>
+          </View>
+        );
+      })}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   content: {
-    paddingHorizontal: 20,
-    marginTop: 12,
+    paddingHorizontal: 18,
+    marginTop: 9,
+    marginBottom: 9,
   },
   title: {
     color: '#fff',
     fontWeight: 'bold',
     fontSize: 18,
     paddingBottom: 3,
+  },
+  block: {
+    flexDirection: 'row',
+    paddingVertical: 3,
+  },
+  blockTitle: {
+    width: '30%',
+  },
+  text: {
+    color: '#fff',
   },
 });
