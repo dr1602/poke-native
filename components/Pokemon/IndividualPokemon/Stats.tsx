7a8e6802b0028d13411bdf0d30b7b31651e75f97
dr1602 <david.arangelg@gmail.com>
2025-07-25 17:29:56 -0600

    feat: bar stats final stats

--- components/Pokemon/IndividualPokemon/Stats.tsx
+++ components/Pokemon/IndividualPokemon/Stats.tsx
@@ -1,10 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { DimensionValue, StyleSheet, Text, View } from 'react-native';
 
 import { usePokemonDetailStore } from '@/store/pokemonDetailStore';
 import { capitalize } from '@/utils/capitalize';
 
 export const Stats = () => {
   const { currentPokemonData } = usePokemonDetailStore();
+
+  const barStyles = (statNumber: number) => {
+    const color =
+      statNumber > 69 ? '#02AC66' : statNumber > 33 ? '#024A86' : '#EF280F';
+
+    return {
+      backgroundColor: color,
+      width: `${statNumber}%` as DimensionValue,
+    };
+  };
 
   return (
     <View style={styles.content}>
@@ -17,7 +27,7 @@ export const Stats = () => {
             <View style={styles.blockInfo}>
               <Text style={styles.number}>{item.base_stat}</Text>
               <View style={styles.backgroundBar}>
-                <View style={styles.bar} />
+                <View style={[styles.bar, barStyles(item.base_stat)]} />
               </View>
             </View>
           </View>
@@ -42,6 +52,7 @@ const styles = StyleSheet.create({
   },
   statName: {
     color: '#fff',
+    fontWeight: 'bold',
     fontSize: 12,
   },
   blockInfo: {
@@ -55,15 +66,13 @@ const styles = StyleSheet.create({
     fontSize: 12,
   },
   backgroundBar: {
-    backgroundColor: '#FA8072',
+    backgroundColor: '#c0c0c0',
     width: '88%',
     height: 6,
     borderRadius: 18,
     overflow: 'hidden',
   },
   bar: {
-    backgroundColor: 'red',
-    width: '100%',
     height: 6,
     borderRadius: 18,
   },
