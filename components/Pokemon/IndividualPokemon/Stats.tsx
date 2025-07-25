847a9b6cc3e53a12423fdacee8e706f43870a51a
dr1602 <david.arangelg@gmail.com>
2025-07-25 17:37:58 -0600

    refactor: separates funtions responsabilities

--- components/Pokemon/IndividualPokemon/Stats.tsx
+++ components/Pokemon/IndividualPokemon/Stats.tsx
@@ -1,26 +1,11 @@
-import { DimensionValue, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 import { usePokemonDetailStore } from '@/store/pokemonDetailStore';
+import { barColours } from '@/utils/barColours';
 import { capitalize } from '@/utils/capitalize';
 
 export const Stats = () => {
   const { currentPokemonData } = usePokemonDetailStore();
-
-  const barStyles = (statNumber: number) => {
-    const color =
-      statNumber > 100
-        ? '#E7D40A'
-        : statNumber > 69
-        ? '#02AC66'
-        : statNumber > 33
-        ? '#024A86'
-        : '#EF280F';
-
-    return {
-      backgroundColor: color,
-      width: `${statNumber}%` as DimensionValue,
-    };
-  };
 
   return (
     <View style={styles.content}>
@@ -33,7 +18,7 @@ export const Stats = () => {
             <View style={styles.blockInfo}>
               <Text style={styles.number}>{item.base_stat}</Text>
               <View style={styles.backgroundBar}>
-                <View style={[styles.bar, barStyles(item.base_stat)]} />
+                <View style={[styles.bar, barColours(item.base_stat)]} />
               </View>
             </View>
           </View>
