2417959264cee52f5c02fb1594eca69d30b76661
dr1602 <david.arangelg@gmail.com>
2025-07-27 13:48:01 -0600

    feat: modifies bar colour fetch function

--- utils/barColours.ts
+++ utils/barColours.ts
@@ -2,13 +2,7 @@ import { DimensionValue } from 'react-native';
 
 export const barColours = (statNumber: number) => {
   const color =
-    statNumber > 100
-      ? '#E7D40A'
-      : statNumber > 69
-      ? '#02AC66'
-      : statNumber > 33
-      ? '#024A86'
-      : '#EF280F';
+    statNumber > 100 ? '#E7D40A' : statNumber > 55 ? '#02AC66' : '#EF280F';
 
   return {
     backgroundColor: color,
