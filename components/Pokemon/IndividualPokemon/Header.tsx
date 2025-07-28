21e970945bacce3874c304a8a9d14e46dde17523
dr1602 <david.arangelg@gmail.com>
2025-07-27 21:13:45 -0600

    feat: adds new header component that has back arrows to go back in route

--- components/Pokemon/IndividualPokemon/Header.tsx
+++ components/Pokemon/IndividualPokemon/Header.tsx
@@ -1,3 +1,28 @@
+import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from 'expo-router';
+import { StyleSheet, View } from 'react-native';
+
 export const Header = () => {
-    return()
-}
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.ButtonContainer}>
+      <AntDesign
+        name='leftcircleo'
+        size={24}
+        color='#fff'
+        style={{ marginLeft: 18, marginTop: 15, zIndex: 10 }}
+        onPress={navigation.goBack}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  ButtonContainer: {
+    position: 'absolute',
+  },
+  PokemonName: {
+    color: 'white',
+  },
+});
