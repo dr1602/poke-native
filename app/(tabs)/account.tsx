c341b65b438f6ea5260baf7a7c32411320b59ad1
dr1602 <david.arangelg@gmail.com>
2025-07-29 15:24:45 -0600

    feat: gives more features to account window

--- app/(tabs)/account.tsx
+++ app/(tabs)/account.tsx
@@ -1,11 +1,22 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const Account = () => {
+  const auth = null;
   return (
     <View>
-      <Text> Account </Text>
+      {auth ? (
+        <Text style={styles.text}> Panel del usuario</Text>
+      ) : (
+        <Text style={styles.text}> Formulario de Login</Text>
+      )}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  text: {
+    color: '#fff',
+  },
+});
 
 export default Account;
