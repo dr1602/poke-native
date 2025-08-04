096d035835961fd4f6af704d4993662ac79d51d8
dr1602 <david.arangelg@gmail.com>
2025-08-03 21:10:11 -0600

    feat: adds styles to Item Menu component

--- components/Auth/ItemMenu.tsx
+++ components/Auth/ItemMenu.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 interface ItemMenuProps {
   title: string;
@@ -8,9 +8,27 @@ interface ItemMenuProps {
 
 export const ItemMenu: React.FC<ItemMenuProps> = ({ title, text }) => {
   return (
-    <View>
-      <Text>{title}</Text>
-      <Text>{text}</Text>
+    <View style={styles.itemMenu}>
+      <Text style={styles.itemMenuTitle}>{title}</Text>
+      <Text style={styles.itemMenuText}>{text}</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  itemMenu: {
+    flexDirection: 'row',
+    paddingVertical: 19,
+    borderBottomWidth: 1,
+    borderColor: '#CFCFCF',
+  },
+  itemMenuTitle: {
+    color: 'white',
+    fontWeight: 'bold',
+    paddingRight: 9,
+    width: 120,
+  },
+  itemMenuText: {
+    color: 'white',
+  },
+});
