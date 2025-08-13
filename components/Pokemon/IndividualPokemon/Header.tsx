32785ad645f115656b7dea2f51d05d71dfa0aabb
dr1602 <david.arangelg@gmail.com>
2025-08-12 21:40:45 -0600

    feat: agrega acceso a store para solo renderizar el componente favoritos cuando la o el usario esté logeado

--- components/Pokemon/IndividualPokemon/Header.tsx
+++ components/Pokemon/IndividualPokemon/Header.tsx
@@ -2,10 +2,13 @@ import { AntDesign } from '@expo/vector-icons';
 import { useNavigation } from 'expo-router';
 import { StyleSheet, View } from 'react-native';
 
+import { useAuthStore } from '@/store/authStore';
 import { Favourites } from './Favourites';
 
 export const Header = () => {
   const navigation = useNavigation();
+
+  const authData = useAuthStore((state) => state.currentAuthData);
 
   return (
     <View style={styles.ButtonContainer}>
@@ -16,7 +19,7 @@ export const Header = () => {
         style={{ marginLeft: 18, marginTop: 15, zIndex: 10 }}
         onPress={navigation.goBack}
       />
-      <Favourites />
+      {authData && <Favourites />}
     </View>
   );
 };
@@ -25,6 +28,9 @@ const styles = StyleSheet.create({
   ButtonContainer: {
     position: 'absolute',
     width: '100%',
+    zIndex: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   PokemonName: {
     color: 'white',
