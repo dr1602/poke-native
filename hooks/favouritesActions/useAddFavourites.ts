b75e61ead467ff17bec57a8f57f2626999075935
dr1602 <david.arangelg@gmail.com>
2025-08-24 23:36:47 -0600

    refactor: updates the add hook so it does not requiere the params to be put inside the hook call

--- hooks/favouritesActions/useAddFavourites.ts
+++ hooks/favouritesActions/useAddFavourites.ts
@@ -1,15 +1,29 @@
 import { useState } from 'react';
 
-import { addFavouritePokemon } from '@/services/favouritesService';
+import {
+  addFavouritePokemon,
+  getFavouritePokemons,
+} from '@/services/favouritesService';
 
-export const useAddFavourites = (pokemonId: number | undefined) => {
+export const useAddFavourites = () => {
   const [isLoadingAddFavourites, setIsLoadingAddFavourites] =
     useState<boolean>(false);
+  const [pokemonFavouritesList, setPokemonFavouritesList] = useState<number[]>(
+    []
+  );
 
-  const addFavourite = async () => {
+  const addFavourite = async (pokemonId: number | undefined) => {
     try {
       setIsLoadingAddFavourites(true);
-      await addFavouritePokemon(pokemonId);
+      if (!pokemonId) return;
+
+      const favourites = await getFavouritePokemons();
+      setPokemonFavouritesList(favourites);
+      const isPokemonSaved = pokemonFavouritesList.includes(pokemonId);
+
+      if (!isPokemonSaved) {
+        await addFavouritePokemon(pokemonId);
+      }
     } catch (error) {
       console.error('Error fetching favourites:', error);
     } finally {
