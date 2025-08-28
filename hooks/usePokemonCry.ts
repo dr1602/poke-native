81ec646bc2eb5f0a6254137629c3555a97faffac
dr1602 <david.arangelg@gmail.com>
2025-08-28 13:01:44 -0600

    feat: adds initial logic to reproduce pokemon cries

---
+++ hooks/usePokemonCry.ts
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import Sound from 'react-native-sound';
+
+export const usePokemonCry = () => {
+  const [isPlayingCry, setIsPlayingCry] = useState<boolean>(false);
+
+  const playCry = (cryUrl: string) => {
+    if (!cryUrl) {
+      console.log('URL de grito no disponible');
+      return;
+    }
+
+    const soundIstance = new Sound(cryUrl, undefined, (error) => {
+      if (error) {
+        console.error('Error al cargar el sonido', error);
+        return;
+      }
+
+      soundIstance.play((success) => {
+        if (success) {
+          setIsPlayingCry(true);
+          console.log('Reproducción exitosa');
+        } else {
+          console.log('Reproducción fallida');
+        }
+        soundIstance.release();
+        setIsPlayingCry(false);
+      });
+    });
+  };
+
+  return { playCry, isPlayingCry };
+};
